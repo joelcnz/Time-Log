3c35cc252d74c5ec469cefa69ffda8f2df8c7f42
Alan Christensen <alan@christensen.org.nz>
2011-06-04 14:10:37 +1200

    added CommandLineParser and Command objects eventually to replace much of code in UserControl.
    TDD one test and commit at at time

---
+++ TimeLog.Tests/CommandlineParserTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TimeLog.Tests
+{
+    [TestFixture]
+    public class CommandlineParserTests
+    {
+        private CommandlineParser commandlineParser;
+
+        [SetUp]
+        public void SetUp()
+        {
+            commandlineParser = new CommandlineParser();
+        }
+
+        [Test]
+        public void EmptyCommand_ReturnsNull()
+        {
+            // Arrange
+
+            // Act
+            var command = commandlineParser.ParseCommandline("");
+
+            // Assert
+            Assert.That(command, Is.Null);
+        }
+
+        [Test, Ignore("Not done yet")]
+        public void AddCommand_ReturnsAddCommand()
+        {
+            // Arrange
+
+            // Act
+            var command = commandlineParser.ParseCommandline("a");
+
+            // Assert
+            Assert.That(command.Root, Is.EqualTo("add"));
+        }
+    }
+}
---
+++ TimeLog/CommandlineParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TimeLog
+{
+    public class Command
+    {
+        public string Root { get; private set; }
+
+        public Command(string root)
+        {
+            Root = root;
+        }
+    }
+
+    public class CommandlineParser
+    {
+     
+        public Command ParseCommandline(string commandline)
+        {
+            return null;
+        }
+
+        private bool IsNumber(char c)
+        {
+            if (c >= '0' && c <= '9')
+                return true;
+
+            return false;
+        }
+
+        private string GetRoot(string input)
+        {
+            var index = 0;
+            for (int i = 0; i < input.Length; ++i)
+                if (input[i] == '"' || IsNumber(input[i]) == true)
+                    break;
+                else
+                    ++index;
+
+            return input.Substring(0, index);
+        }
+    }
+}
