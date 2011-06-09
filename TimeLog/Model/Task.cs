cfb0b9805f3c827f91fba2247a4e021f6415e1de
Joel Christensen <joelcnz@gmail.com>
2011-06-09 15:24:58 +1200

    Changed class return string id + title. Showed things wrong

--- TimeLog/Model/Task.cs
+++ TimeLog/Model/Task.cs
@@ -1,18 +1,19 @@
-﻿//#I don't know the order
-//#not sure about this
-//#how do I sort a list of instances of Task?
+﻿//#in wrong class
+//#This should not be here! List of tasks to choose from, and list of added tasks.
 using System;
 
 namespace TimeLog.Model
 {
+    //#date and time does not belong to task
+    // task just has Id and Title
     public class Task : IComparable<Task>
     {
+        //#in wrong class
         private Period period;
-
         public string Title { get; private set; }
-
+        //#in wrong class
         public DateTime DateTime { get; private set; }
-
+        //#in wrong class
         public Period Period
         {
             get { return period; }
@@ -20,17 +21,29 @@ namespace TimeLog.Model
 
         public int Id { get; set; }
 
+        //#should be Id, and title
         public Task(string title, DateTime dateTime)
         {
             Title = title;
-            this.DateTime = dateTime; //#not sure about this
+            DateTime = dateTime; //#in wrong class
         }
 
+        /// <summary>
+        /// Get single task that is displayed in list of tasks for adding
+        /// </summary>
+        /// <returns>id and title</returns>
         public override string ToString()
         {
-            return DateTime.ToString();
+            return Id + " - " + Title;
         }
 
+        /// <summary>
+        /// For ordering by date added tasks
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        //#This should not be here! List of tasks to choose from, and list of added dones.
+        // I don't know what to call the different things
         public int CompareTo(Task other)
         {
             return DateTime.CompareTo(other.DateTime);
